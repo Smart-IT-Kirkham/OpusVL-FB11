c82dd80c11e224dc5b296303d83b8ad6b7346beb
jj <jj@c90024e5-1ee7-0310-811b-ed421df6bfac>
2010-06-17 13:08:03 +0000

    Added PDF view to SVN
    git-svn-id: https://svn/svn/Development/OpusVL/CatalystX/AppKit@6826 c90024e5-1ee7-0310-811b-ed421df6bfac

---
+++ lib/OpusVL/AppKit/View/PDF/Reuse.pm
@@ -0,0 +1,29 @@
+package OpusVL::AppKit::View::PDF::Reuse;
+
+use strict;
+use base 'Catalyst::View::PDF::Reuse';
+
+=head1 NAME
+
+OpusVL::AppKit::View::PDF::Reuse - PDF::Reuse View for OpusVL::AppKit
+
+=head1 DESCRIPTION
+
+PDF::Reuse View for OpusVL::AppKit. 
+
+=head1 AUTHOR
+
+Jon Allen
+
+=head1 SEE ALSO
+
+L<OpusVL::AppKit>
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
