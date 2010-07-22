456f9993a5a47759a2d06e6349026b07c818aba6
jj <jj@c90024e5-1ee7-0310-811b-ed421df6bfac>
2010-07-22 10:34:30 +0000

    Bumped version to 1.01
    git-svn-id: https://svn/svn/Development/OpusVL/CatalystX/AppKit@6941 c90024e5-1ee7-0310-811b-ed421df6bfac

--- lib/OpusVL/AppKit.pm
+++ lib/OpusVL/AppKit.pm
@@ -76,7 +76,7 @@ package OpusVL::AppKit;
 use strict;
 use warnings;
 use OpusVL::AppKit::Builder;
-our $VERSION = '1.00';
+our $VERSION = '1.01';
 
 ##################################################################################################################################
 # main #
