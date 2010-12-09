66ad8ff5dfb134d13e556b22d835e2186ac468aa
colin <colin@c90024e5-1ee7-0310-811b-ed421df6bfac>
2010-12-09 09:10:28 +0000

    Added a dummy version number to the test app
    git-svn-id: https://svn/svn/Development/OpusVL/CatalystX/AppKit@7809 c90024e5-1ee7-0310-811b-ed421df6bfac

--- t/lib/TestApp.pm
+++ t/lib/TestApp.pm
@@ -3,9 +3,11 @@ package TestApp;
 use strict;
 use warnings;
 
+our $VERSION = '0.3';
+
 use TestApp::Builder;
 
-TestApp::Builder->new( appname => __PACKAGE__ )->bootstrap;
+TestApp::Builder->new( appname => __PACKAGE__, version => $VERSION )->bootstrap;
 
 
 1;
