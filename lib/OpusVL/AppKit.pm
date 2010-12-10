d0bd1e0b2b9b6fd5985c23e3a0a189515e644f1c
jj <jj@c90024e5-1ee7-0310-811b-ed421df6bfac>
2010-12-10 11:31:27 +0000

    Bumped version to 1.08
    git-svn-id: https://svn/svn/Development/OpusVL/CatalystX/AppKit@7847 c90024e5-1ee7-0310-811b-ed421df6bfac

--- lib/OpusVL/AppKit.pm
+++ lib/OpusVL/AppKit.pm
@@ -78,7 +78,7 @@ This software is licensed according to the "IP Assignment Schedule" provided wit
 use strict;
 use warnings;
 use OpusVL::AppKit::Builder;
-our $VERSION = '1.07';
+our $VERSION = '1.08';
 
 ##################################################################################################################################
 # main #
