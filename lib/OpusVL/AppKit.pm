14089fbef99b98919e53bb7abc738aef10a463bd
colin <colin@c90024e5-1ee7-0310-811b-ed421df6bfac>
2010-12-21 11:49:25 +0000

    Rolled version number on
    git-svn-id: https://svn/svn/Development/OpusVL/CatalystX/AppKit@7977 c90024e5-1ee7-0310-811b-ed421df6bfac

--- lib/OpusVL/AppKit.pm
+++ lib/OpusVL/AppKit.pm
@@ -78,7 +78,7 @@ This software is licensed according to the "IP Assignment Schedule" provided wit
 use strict;
 use warnings;
 use OpusVL::AppKit::Builder;
-our $VERSION = '1.12';
+our $VERSION = '1.13';
 
 ##################################################################################################################################
 # main #
