7c9fa87b31155a3cf15c8329f312e312610324b8
Colin Newell <colin@opusvl.com>
2011-03-09 17:27:45 +0000

    Added more attributes

--- t/lib/TestX/CatalystX/ExtensionA/Controller/ExtensionA.pm
+++ t/lib/TestX/CatalystX/ExtensionA/Controller/ExtensionA.pm
@@ -8,7 +8,7 @@ with 'OpusVL::AppKit::RolesFor::Controller::GUI';
 __PACKAGE__->config
 (
     appkit_name                 => 'ExtensionA',
-    appkit_order                => 1,
+    appkit_order                => 10,
     appkit_icon                 => 'static/images/flagA.jpg',
     appkit_myclass              => 'TestX::CatalystX::ExtensionA',
     appkit_method_group         => 'Extension A',
@@ -21,6 +21,7 @@ sub home
     :Args(0)
     :NavigationHome
     :AppKitFeature('Extension A')
+    :AppKitRolesAllowed('Administrator')
 {
     my ($self, $c) = @_;
     $c->stash->{template} = 'extensiona.tt';
