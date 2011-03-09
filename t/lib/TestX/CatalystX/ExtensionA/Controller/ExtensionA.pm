40657b92998779da2e9c7972ee31ada8a409c5f4
Colin Newell <colin@opusvl.com>
2011-03-09 17:14:33 +0000

    Decorated a couple of controllers with the attributes I think we want to use for the new features

--- t/lib/TestX/CatalystX/ExtensionA/Controller/ExtensionA.pm
+++ t/lib/TestX/CatalystX/ExtensionA/Controller/ExtensionA.pm
@@ -8,14 +8,19 @@ with 'OpusVL::AppKit::RolesFor::Controller::GUI';
 __PACKAGE__->config
 (
     appkit_name                 => 'ExtensionA',
+    appkit_order                => 1,
     appkit_icon                 => 'static/images/flagA.jpg',
     appkit_myclass              => 'TestX::CatalystX::ExtensionA',
+    appkit_method_group         => 'Extension A',
+    appkit_method_group_order   => 2,
+    appkit_shared_module        => 'ExtensionA',
 );
 
 sub home
     :Path
     :Args(0)
     :NavigationHome
+    :AppKitFeature('Extension A')
 {
     my ($self, $c) = @_;
     $c->stash->{template} = 'extensiona.tt';
