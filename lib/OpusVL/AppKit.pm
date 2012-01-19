3e3ecc20701c2d9c9a6f11d9e00188f4c353f3e6
Brad <brad@geeksware.net>
2012-01-19 08:33:51 +0000

    Added a change to intranet_wrapper.tt to check for a specific Customer to enable the Notifications (Added for Eazy Collect)

--- lib/OpusVL/AppKit.pm
+++ lib/OpusVL/AppKit.pm
@@ -78,7 +78,7 @@ This software is licensed according to the "IP Assignment Schedule" provided wit
 use strict;
 use warnings;
 use OpusVL::AppKit::Builder;
-our $VERSION = '1.44';
+our $VERSION = '1.45';
 
 ##################################################################################################################################
 # main #
