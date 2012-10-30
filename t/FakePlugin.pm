09230ec6736e0383e275e5ffd6b720190f00cc21
Colin Newell <colin@opusvl.com>
2012-10-30 16:48:48 +0000

    Started to add model

--- t/FakePlugin.pm
+++ t/FakePlugin.pm
@@ -3,12 +3,24 @@ package FakePlugin;
 use Moose;
 use FakeCache;
 use FakeLog;
+use Carp;
 extends 'OpusVL::AppKit::Plugin::AppKit';
 
 has debug => (isa => 'Bool', is => 'ro', default => '0');
 has config => (isa => 'HashRef', is => 'ro', default => sub { {} } );
 has cache => (is => 'ro', default => sub { FakeCache->new() } );
 has components => (is => 'ro', isa => 'HashRef', default => sub { {} } );
+has schema => (is => 'ro');
+
+sub model
+{
+    my $self = shift;
+    my $model = shift;
+    my ($rs_name) = $model =~ /\:\:(.*)$/;
+    my $rs = $self->schema($rs_name);
+    croak "Unable to find model $model" unless $rs;
+    return $rs;
+}
 
 has log => (is => 'ro', default => sub { FakeLog->new() } );
 
