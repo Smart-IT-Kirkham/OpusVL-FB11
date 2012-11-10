e03b3ec021b156a61ef59d67c8e42a0c5b5844bd
Colin Newell <colin.newell@gmail.com>
2012-11-10 11:21:24 +0000

    Added documentation for new role

--- lib/OpusVL/AppKit/RolesFor/UserSetupResultSet.pm
+++ lib/OpusVL/AppKit/RolesFor/UserSetupResultSet.pm
@@ -1,5 +1,82 @@
 package OpusVL::AppKit::RolesFor::UserSetupResultSet;
 
+=head1 NAME
+
+    OpusVL::AppKit::RolesFor::UserSetupResultSet
+
+=head1 SYNOPSIS
+
+    package OpusVL::App::Schema::ResultSet::Role;
+
+    use Moose;
+    extends 'DBIx::Class::ResultSet';
+    with 'OpusVL::AppKit::RolesFor::UserSetupResultSet';
+
+    sub initdb
+    {
+        my $self = shift;
+
+        my $permissions = {
+            "AppKit/Home Page" => ["Admin"],
+            "AppKit/Password Change" => ["Admin"],
+            ...
+        };
+        my $users = [
+            { 
+                username => 'appkitadmin',
+                password => 'password',
+                ...
+            }
+        ];
+        $self->setup_users($users, $permissions, 'Admin');
+    }
+
+=head1 DESCRIPTION
+
+This role provides a simple way to setup default users and set of roles
+for an AppKit auth database.  The idea is to call it from an initdb function
+which gets called when the database is built by your script.
+
+=head1 METHODS
+
+=head2 setup_users
+
+    my $permissions = {
+        "AppKit/Home Page" => ["Admin"],
+        "AppKit/Password Change" => ["Admin"],
+        "AppKit/Role Administration" => ["Admin"],
+        "AppKit/User Administration" => ["Admin"],
+        "AppKit/User Password Administration" => ["Admin"],
+        "Search/Search box" => ["Admin"],
+        "System Parameters/System Parameters" => ["Admin"],
+    };
+    my $users = [
+        { 
+            username => 'appkitadmin',
+            password => 'password',
+            email => 'admin@opusvl.com',
+            name => 'AppKit Admin',
+            tel => '01788 550 302',
+        }
+    ];
+    $self->setup_users($users, $permissions, 'Admin');
+
+This method takes a hash containing the feature permissions and a list
+of users and the role to give those users, and creates the permissions
+and users.
+
+To generate the permissions hash use the L<scripts/permission_extract.sh>
+script.
+
+=head1 COPYRIGHT and LICENSE
+
+Copyright (C) 2012 OpusVL
+
+This software is licensed according to the "IP Assignment Schedule" provided with the development project.
+
+=cut
+
+
 use Moose::Role;
 use List::MoreUtils qw/uniq/;
 
