9adcead12123208318d10641d674d4202001d5de
Colin Newell <colin@opusvl.com>
2012-11-14 11:32:54 +0000

    Improved the interface and added the ability to setup the old style rules too

--- lib/OpusVL/AppKit/RolesFor/UserSetupResultSet.pm
+++ lib/OpusVL/AppKit/RolesFor/UserSetupResultSet.pm
@@ -18,17 +18,27 @@ package OpusVL::AppKit::RolesFor::UserSetupResultSet;
 
         my $permissions = {
             "AppKit/Home Page" => ["Admin"],
-            "AppKit/Password Change" => ["Admin"],
             ...
+            "System Parameters/System Parameters" => ["Admin"],
+        };
+        my $role_permissions = {
+            "modules/audittrail/events/all_events" => ["Admin"],
+            ...
+            "modules/resultsetsearch/search_results" => ["Admin"],
         };
         my $users = [
             { 
                 username => 'appkitadmin',
-                password => 'password',
                 ...
+                name => 'AppKit Admin',
+                roles => [ 'Admin' ],
             }
         ];
-        $self->setup_users($users, $permissions, 'Admin');
+        $self->setup_users({
+            users => $users, 
+            feature_permissions => $permissions,
+            role_permissions => $role_permissions,
+        });
     }
 
 =head1 DESCRIPTION
@@ -50,16 +60,25 @@ which gets called when the database is built by your script.
         "Search/Search box" => ["Admin"],
         "System Parameters/System Parameters" => ["Admin"],
     };
+    my $role_permissions = {
+        "modules/audittrail/events/all_events" => ["Admin"],
+        "modules/audittrail/events/event_search" => ["Admin"],
+        "modules/audittrail/events/home" => ["Admin"],
+        "modules/resultsetsearch/search_results" => ["Admin"],
+    };
     my $users = [
         { 
             username => 'appkitadmin',
-            password => 'password',
-            email => 'admin@opusvl.com',
+            ...
             name => 'AppKit Admin',
-            tel => '01788 550 302',
+            roles => [ 'Admin' ],
         }
     ];
-    $self->setup_users($users, $permissions, 'Admin');
+    $self->setup_users({
+        users => $users, 
+        feature_permissions => $permissions,
+        role_permissions => $role_permissions,
+    });
 
 This method takes a hash containing the feature permissions and a list
 of users and the role to give those users, and creates the permissions
@@ -83,22 +102,35 @@ use List::MoreUtils qw/uniq/;
 sub setup_users
 {
     my $self = shift;
-    my $user_list = shift;
-    my $permissions_list = shift;
-    my $user_role = shift;
+    my $args = shift;
+    my $user_list = $args->{users};
+    my $feature_permissions_list = $args->{feature_permissions};
+    my $role_permission_list = $args->{role_permissions};
 
     my $schema = $self->result_source->schema;
-    my @roles = uniq map { @$_ } values $permissions_list;
+    my @roles = uniq map { @$_ } values %$feature_permissions_list;
+    push @roles, uniq map { @$_ } values %$role_permission_list;
+    @roles = uniq @roles;
     my %role_map;
     for my $role (@roles)
     {
         $role_map{$role} = $schema->resultset('Role')->find_or_create({ role => $role });
     }
     my $feature_rs = $schema->resultset('Aclfeature');
-    for my $feature (keys %$permissions_list)
+    for my $feature (keys %$feature_permissions_list)
     {
         my $f = $feature_rs->find_or_create({ feature => $feature });
-        for my $role (@{$permissions_list->{$feature}})
+        for my $role (@{$feature_permissions_list->{$feature}})
+        {
+            my $role_o = $role_map{$role};
+            $f->add_to_roles($role_o);
+        }
+    }
+    my $rule_rs = $schema->resultset('Aclrule');
+    for my $rule (keys %$role_permission_list)
+    {
+        my $f = $rule_rs->find_or_create({ actionpath => $rule });
+        for my $role (@{$role_permission_list->{$rule}})
         {
             my $role_o = $role_map{$role};
             $f->add_to_roles($role_o);
@@ -106,12 +138,13 @@ sub setup_users
     }
     my @users;
     for my $user_data (@$user_list) {
-        push @users, $schema->resultset('User')->create($user_data);
-    }
-    my $admin = $role_map{$user_role};
-    for my $user (@users)
-    {
-        $user->add_to_roles($admin);
+        my $roles_wanted = delete $user_data->{roles};
+        my $user = $schema->resultset('User')->create($user_data);
+        for my $wanted (@$roles_wanted)
+        {
+            my $role = $role_map{$wanted};
+            $user->add_to_roles($role);
+        }
     }
 }
 
