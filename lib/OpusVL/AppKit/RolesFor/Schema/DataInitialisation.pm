b36f100c3716cbb7b03d230d40663935a1b763c2
Colin Newell <colin.newell@gmail.com>
2012-11-30 19:43:44 +0000

    Added documentation

--- lib/OpusVL/AppKit/RolesFor/Schema/DataInitialisation.pm
+++ lib/OpusVL/AppKit/RolesFor/Schema/DataInitialisation.pm
@@ -16,3 +16,40 @@ sub deploy_with_data
 }
 
 1;
+
+=head1 NAME
+
+OpusVL::AppKit::RolesFor::Schema::DataInitialisation - Deploy with data
+
+=head1 DESCRIPTION
+
+This role allows you to extend your DBIx::Class::Schema with a deploy_with_data
+method that can be used instead of deploy to deploy and populate your database
+with some initial data.  This is typically used to put a basic user into an auth
+database and setting up any standard system parameters that are required.
+
+    package My::Schema;
+    use Moose;
+    use namespace::autoclean;
+    extends 'DBIx::Class::Schema';
+    with 'OpusVL::AppKit::RolesFor::Schema::DataInitialisation';
+    __PACKAGE__->load_namespaces;
+    __PACKAGE__->meta->make_immutable;
+    1;
+
+
+=head1 METHODS
+
+=head2 deploy_with_data
+
+This method does a deploy and then checks your resultsets for initdb methods to call.
+If they are found they are called.  This allows you to populate your newly deployed
+database with initial data.
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2012 OpusVL.
+
+This software is licensed according to the "IP Assignment Schedule" provided with the development project.
+
+=cut
