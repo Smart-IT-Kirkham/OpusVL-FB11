3bde053281c65b9fbcddc06d7fba74b469bd006d
Colin Newell <colin@opusvl.com>
2014-07-17 10:03:33 +0100

    Bumped version number

--- lib/OpusVL/AppKit.pm
+++ lib/OpusVL/AppKit.pm
@@ -78,7 +78,7 @@ This software is licensed according to the "IP Assignment Schedule" provided wit
 use strict;
 use warnings;
 use OpusVL::AppKit::Builder;
-our $VERSION = '2.03';
+our $VERSION = '2.04';
 
 ##################################################################################################################################
 # main #
