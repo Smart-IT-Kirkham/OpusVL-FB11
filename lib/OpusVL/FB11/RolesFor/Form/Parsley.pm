58ee431f4699c2af9ee16983f2752ba935ededca
Nick Booker <nick.booker@opusvl.com>
2015-12-04 16:45:49 +0000

    Note issue with buttons called 'submit'

--- lib/OpusVL/FB11/RolesFor/Form/Parsley.pm
+++ lib/OpusVL/FB11/RolesFor/Form/Parsley.pm
@@ -15,11 +15,28 @@ OpusVL::FB11::RolesFor::Form::Parsley - Use Parsley to validate the form
     use OpusVL::FB11::Plugin::FormHandler;
     with 'OpusVL::FB11::RolesFor::Form::Parsley';
 
+    # ...
+
+    # Important thing to note is that button name is 'submit_it' not 'submit'.
+    # I think anything else will do, e.g. 'save'
+    has_field submit_it => (
+        type => 'Submit',
+        widget => 'ButtonTag',
+        widget_wrapper => 'None',
+        value => '<i class="fa fa-floppy-o"></i> Save',
+        element_attr => { class => ['btn', 'btn-primary']},
+    );
+
+
 =head1 DESCRIPTION
 
 Use with L<OpusVL::FB11::Plugin::FormHander> to activate Parsley validation for your form.
 
 If doing it manually in HTML, at the time of writing, you would add C<class="parsley"> to your C<form> tag.
 This saves you having to craft your own C<form> tag - just C<[% form.render | none%]> will do.
+
+Also remember to never call your submit button C<submit>.
+
+Call it something like C<save> or C<submit_it> instead.
 
 =cut
