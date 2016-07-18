ec5743f6ccf6d9e929ab1b81e466ff7c10143f35
Colin Newell <colin@opusvl.com>
2016-07-18 11:35:50 +0100

    Bumped version number

--- lib/OpusVL/FB11/Schema/FB11AuthDB.pm
+++ lib/OpusVL/FB11/Schema/FB11AuthDB.pm
@@ -6,7 +6,7 @@ use warnings;
 use Moose;
 extends 'DBIx::Class::Schema';
 
-our $VERSION='1';
+our $VERSION = '0.024';
 
 with 'OpusVL::DBIC::Helper::RolesFor::Schema::DataInitialisation';
 with 'OpusVL::FB11::RolesFor::Schema::FB11AuthDB';
