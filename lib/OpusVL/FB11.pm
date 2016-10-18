0aaa2b520e997485dd677ae8c1fec5d1b9995d09
Alastair McGowan-Douglas <alastair.mcgowan@opusvl.com>
2016-10-18 15:26:33 +0100

    Bumped version number

--- lib/OpusVL/FB11.pm
+++ lib/OpusVL/FB11.pm
@@ -78,7 +78,7 @@ This software is licensed according to the "IP Assignment Schedule" provided wit
 use strict;
 use warnings;
 use OpusVL::FB11::Builder;
-our $VERSION = '0.026';
+our $VERSION = '0.027';
 
 ##################################################################################################################################
 # main #
--- lib/OpusVL/FB11/Schema/FB11AuthDB.pm
+++ lib/OpusVL/FB11/Schema/FB11AuthDB.pm
@@ -6,7 +6,7 @@ use warnings;
 use Moose;
 extends 'DBIx::Class::Schema';
 
-our $VERSION = '0.026';
+our $VERSION = '0.027';
 
 with 'OpusVL::DBIC::Helper::RolesFor::Schema::DataInitialisation';
 with 'OpusVL::FB11::RolesFor::Schema::FB11AuthDB';
