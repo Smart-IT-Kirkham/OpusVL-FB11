c1466bb085ac194b988a3bcfaaeee173554b6933
Alastair McGowan-Douglas <alastair.mcgowan@opusvl.com>
2017-07-24 13:58:55 +0100

    Bumped version number

--- lib/OpusVL/FB11/Schema/FB11AuthDB.pm
+++ lib/OpusVL/FB11/Schema/FB11AuthDB.pm
@@ -6,7 +6,7 @@ use warnings;
 use Moose;
 extends 'DBIx::Class::Schema';
 
-our $VERSION = '0.034';
+our $VERSION = '0.035';
 
 with 'OpusVL::DBIC::Helper::RolesFor::Schema::DataInitialisation';
 with 'OpusVL::FB11::RolesFor::Schema::FB11AuthDB';
