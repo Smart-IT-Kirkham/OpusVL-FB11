4ca38af644461768f5ab4502e78c1897beae6dc4
Alastair McGowan-Douglas <alastair.mcgowan@opusvl.com>
2017-10-12 16:42:18 +0100

    Added sysparams model

---
+++ lib/OpusVL/SysParams/Schema.pm
@@ -0,0 +1,18 @@
+package OpusVL::SysParams::Schema;
+
+=head1 SYNOPSIS
+
+This is the DBIx::Class schema for the SysParams module.
+
+=cut
+
+use Moose;
+use namespace::autoclean;
+extends 'DBIx::Class::Schema';
+
+__PACKAGE__->load_namespaces;
+
+__PACKAGE__->meta->make_immutable(inline_constructor => 0);
+
+1;
+
