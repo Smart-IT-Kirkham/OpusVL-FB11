185db702bb97f49c13bbb0758485c6be9ee22202
Alastair McGowan-Douglas <alastair.douglas@gmail.com>
2018-04-30 12:32:42 +0100

    Implement OpusVL::Parameters for preferences

--- lib/OpusVL/FB11/Schema/Parameters/Result/User.pm
+++ lib/OpusVL/FB11/Schema/Parameters/Result/User.pm
@@ -3,6 +3,8 @@ package OpusVL::FB11::Schema::Parameters::Result::User;
 use Moose;
 use MooseX::NonMoose;
 
+with 'OpusVL::Preferences::RolesFor::Result::PrfOwner';
+
 use DBIx::Class::Candy
     -components => ['InflateColumn::Serializer'];
 
@@ -12,11 +14,25 @@ primary_column id => {
     data_type => 'int',
 };
 
-column prefs_json => {
-    data_type => 'jsonb',
-    serializer_class => 'JSON',
+column prf_owner_type_id => {
+    data_type      => 'integer',
+    is_nullable    => 1,
+    is_foreign_key => 1
 };
 
-belongs_to user => 'OpusVL::FB11::Schema::FB11AuthDB::Result::User' => 'id';
+
+belongs_to prf_owner => 'OpusVL::Preferences::Schema::Result::PrfOwner',
+    {
+        'foreign.prf_owner_id'      => 'self.id',
+        'foreign.prf_owner_type_id' => 'self.prf_owner_type_id'
+    };
+
+belongs_to prf_owner_type => 'OpusVL::Preferences::Schema::Result::PrfOwnerType',
+    {
+        'foreign.prf_owner_type_id' => 'self.prf_owner_type_id'
+    };
+
+
+belongs_to core_user => 'OpusVL::FB11::Schema::FB11AuthDB::Result::User' => 'id';
 
 1;
