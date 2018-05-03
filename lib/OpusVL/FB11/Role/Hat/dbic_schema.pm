c8b559f689684e44129b471b6b3eff18ae7f9562
Alastair McGowan-Douglas <alastair.douglas@gmail.com>
2018-05-03 15:24:59 +0100

    Hats for DBIC

---
+++ lib/OpusVL/FB11/Role/Hat/dbic_schema.pm
@@ -0,0 +1,22 @@
+package OpusVL::FB11::Role::Hat::dbic_schema;
+
+# ABSTRACT: A brain wearing this hat can provide a DBIC schema
+
+use Moose::Role;
+with "OpusVL::FB11::Role::Hat";
+
+requires 'schema';
+
+1;
+
+=head1 DESCRIPTION
+
+Your brain can provide a hat that can provide a DBIx::Class::Schema.
+
+See also L<OpusVL::FB11::Hat::dbic_schema::is_brain>.
+
+=head1 METHODS
+
+=head2 schema
+
+Must return a DBIx::Class::Schema.
