13677eb643601a1f303aa26d71e4662d0d3163ed
Alastair McGowan-Douglas <alastair.douglas@gmail.com>
2018-05-03 15:24:59 +0100

    Hats for DBIC

---
+++ lib/OpusVL/FB11/Hat/dbic_schema/is_brain.pm
@@ -0,0 +1,17 @@
+package OpusVL::FB11::Hat::dbic_schema::is_brain;
+
+# ABSTRACT: a dbic_schema hat where the brain is the schema
+use Moose;
+with "OpusVL::FB11::Role::Hat::dbic_schema";
+
+sub schema { shift->__brain }
+
+1;
+
+=head1 DESCRIPTION
+
+A brain can say it wears the dbic_schema hat and then you have to implement how.
+
+If you say you wear the dbic_schema::is_brain hat, you're just saying that your
+brain and your schema are the same object, so this just does that.
+
