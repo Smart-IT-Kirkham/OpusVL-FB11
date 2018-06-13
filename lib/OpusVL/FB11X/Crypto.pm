257c8e2f23ce70a01813fd971b46ded5f876194c
Alastair Douglas <alastair.douglas@gmail.com>
2018-06-13 10:39:32 +0100

    crypto hat

---
+++ lib/OpusVL/FB11X/Crypto.pm
@@ -0,0 +1,19 @@
+package OpusVL::FB11X::Crypto;
+
+with 'OpusVL::FB11::RolesFor::Plugin';
+
+our $VERSION = '0.036';
+
+after 'setup_components' => sub {
+    my $class = shift;
+
+    my $moduledir = $class->add_paths(__PACKAGE__);
+
+    CatalystX::InjectComponent->inject(
+        into      => $class,
+        component => 'OpusVL::FB11X::Crypto::Model::Crypto',
+        as        => 'Model::Crypto'
+    );
+}
+
+1;
