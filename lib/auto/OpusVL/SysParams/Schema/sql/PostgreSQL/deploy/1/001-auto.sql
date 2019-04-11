2f286cb13ac9b89b2882a897bcee43e22c7df4ba
Alastair Douglas <alastair.douglas@gmail.com>
2019-04-11 14:05:44 +0100

    Create new schema version
    This munges the first version to avoid deploying a table we don't need
    any more, without affecting anyone who has already deployed this table.

--- lib/auto/OpusVL/SysParams/Schema/sql/PostgreSQL/deploy/1/001-auto.sql
+++ lib/auto/OpusVL/SysParams/Schema/sql/PostgreSQL/deploy/1/001-auto.sql
@@ -1,18 +1,1 @@
--- 
--- Created by SQL::Translator::Producer::PostgreSQL
--- Created on Wed Jun 13 09:36:48 2018
--- 
-;
---
--- Table: sys_info
---
-CREATE TABLE "sys_info" (
-  "name" text NOT NULL,
-  "label" text,
-  "value" text,
-  "comment" text,
-  "data_type" character varying,
-  PRIMARY KEY ("name")
-);
-
-;
+-- NO OP - see upgrade/1-2/001-auto.sql
