485a8d0caf4f067ea323a0a16419a6e24e81150f
Alastair Douglas <alastair.douglas@gmail.com>
2019-04-11 14:18:16 +0100

    Add documentation for the strategy we care about

--- lib/OpusVL/SysParams/Strategy/Namespaced.pm
+++ lib/OpusVL/SysParams/Strategy/Namespaced.pm
@@ -5,6 +5,46 @@ use v5.24;
 use Moose;
 with 'OpusVL::SysParams::Role::Strategy';
 
+=head1 DESCRIPTION
+
+This strategy uses the component name as a namespace for the system parameters.
+
+It is perfectly acceptable, nay encouraged, to use the double-colon separator in
+the component if it is useful to do so.
+
+=head1 SYNPOSIS
+
+    my $sysparams = OpusVL::FB11::Hive->service('sysparams')
+        ->for_component('fb11::base')
+
+This strategy is installed as the default provider for the C<sysparams> service
+when you use L<OpusVL::SysParams> as the brain. See the documentation for that
+module for how to select the brain for this service.
+
+=head1 PROPERTIES
+
+=head2 namespace
+
+The namespace to search for parameters. When you use the Hive, as in the
+synopsis, this comes from what you passed to
+L<OpusVL::SysParams::Hat::sysparams::namespaced/for_component>.
+
+=head2 schema
+
+This is by default an instance of L<OpusVL::SysParams::Schema>, constructed from
+L<OpusVL::SysParams/connect_info>. You may provide any connected schema for
+testing purposes, e.g.
+
+=head1 METHODS
+
+See also L<OpusVL::SysParams::Role::Strategy>.
+
+=head2 value_of
+
+Returns the value of the provided parameter within the namespace in C<namespace>.
+
+=cut
+
 has component => (
     is => 'ro'
 );
