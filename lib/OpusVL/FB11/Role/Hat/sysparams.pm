c12ffa57ed604d565e44022ce9c4a761c2dc1d86
Alastair Douglas <alastair.douglas@gmail.com>
2019-04-17 14:56:08 +0100

    Fix a doc

--- lib/OpusVL/FB11/Role/Hat/sysparams.pm
+++ lib/OpusVL/FB11/Role/Hat/sysparams.pm
@@ -7,11 +7,12 @@ with "OpusVL::FB11::Role::Hat";
 
 =head1 DESCRIPTION
 
-A sysparams hat is actually a proxy because different components might have
-their sysparams stored differently. The core behaviour for sysparams is that
-each parameter will be stored the same but namespaced by the component's
-friendly name. This architecture allows us to support different storage or
-legacy data that doesn't have any namespacing in it.
+The core behaviour for sysparams is that each parameter will be stored the same
+way (i.e. all in the same table), but namespaced by the component's friendly
+name.
+
+We use the hat to take in your component name and return an object that
+understands the namespace idea.
 
 The current implementation of sysparams is in L<OpusVL::SysParams>, which is
 bundled with FB11, so you should be able to just use that as your brain and
