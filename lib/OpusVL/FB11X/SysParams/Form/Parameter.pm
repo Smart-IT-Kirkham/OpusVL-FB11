221c367b57704b2022b66c4599c6e223cb8159b4
Alastair Douglas <alastair.douglas@gmail.com>
2019-06-25 18:39:43 +0100

    Editable sysparams

--- lib/OpusVL/FB11X/SysParams/Form/Parameter.pm
+++ lib/OpusVL/FB11X/SysParams/Form/Parameter.pm
@@ -11,8 +11,29 @@ use OpusVL::FB11::Plugin::FormHandler;
 
 has_field name  => ( type => 'Display', label => 'Parameter' );
 has_field label => ( type => 'Display', label => 'Label' );
-has_field comment => ( type => 'Text' );
-has_field value => ( type => 'Text' );
+has_field comment => ( type => 'Display', label => 'Comment' );
+
+# I couldn't see a tidy way of creating a field that could be a repeatable or a
+# single field depending on whether I wanted it to be an array or not. So I
+# created two fields, and the controller picks.
+has_field value => ( type => 'Text', do_label => 0 );
+
+# "contains" is a special name for repeatable sub-fields; see
+# HTML::FormHandler::Manual::Fields
+has_field values => (
+    type => 'Repeatable',
+    do_wrapper => 0,
+);
+has_field 'values.contains' => (
+    do_label => 0,
+    do_wrapper => 0,
+    type => 'Text',
+    element_attr => {
+        class => 'js-repeatable',
+        'data-repeatable-format' => '(values\.)(\d+)'
+    }
+);
+
 has_field submitbutton => (
     type    => 'Submit',
     widget  => 'ButtonTag',
@@ -29,6 +50,13 @@ sub html_name {
 sub html_label {
     my ($self, $field) = @_;
     return "<h4>" . $field->value . "</h4>";
+}
+
+sub html_comment {
+    my ($self, $field) = @_;
+    return '' unless $field->value;
+    return "<p>" . $field->value . "</p>";
+
 }
 
 no HTML::FormHandler::Moose;
