fde47d43969ecfcf1e6e911fe273b3e0d63fa7f6
Alastair Douglas <alastair.douglas@gmail.com>
2019-06-27 18:13:27 +0100

    Gather custom parameter schemata

--- lib/OpusVL/CustomParams/Hat/objectparams/extender.pm
+++ lib/OpusVL/CustomParams/Hat/objectparams/extender.pm
@@ -3,11 +3,31 @@ package OpusVL::CustomParams::Hat::objectparams::extender;
 # ABSTRACT: Provides schemas to ObjectParams by getting them from a database
 our $VERSION = '0';
 
+use List::Gather;
+use OpusVL::FB11::Hive;
 use Moose;
 with 'OpusVL::ObjectParams::Role::Hat::objectparams::extender';
 
 sub schemas {
     my $self = shift;
+
+    # This seems like it shouldn't be done this way. Surely the brain can get at
+    # its own hat?! We should look at fixing that in the Hive.
+    my $customparams_hat = OpusVL::FB11::Hive->hat($self->__brain, 'customparams');
+
+    return gather {
+        for my $type ($customparams_hat->available_types) {
+            my $schema = $customparams_hat->get_schema_for($type)
+            or next;
+
+            take ($type => {
+                type => 'object',
+                title => "User-defined parameters",
+                'x-namespace' => $self->parameter_owner_identifier,
+                properties => $schema
+            })
+        }
+    }
 }
 
 1;
