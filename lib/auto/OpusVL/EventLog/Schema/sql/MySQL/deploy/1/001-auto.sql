d635932b914704e59601601e2472fff114fb807a
Alastair Douglas <alastair.douglas@gmail.com>
2019-07-03 11:32:49 +0100

    Introduce tags and put environmental data into that

--- lib/auto/OpusVL/EventLog/Schema/sql/MySQL/deploy/1/001-auto.sql
+++ lib/auto/OpusVL/EventLog/Schema/sql/MySQL/deploy/1/001-auto.sql
@@ -1,6 +1,6 @@
 -- 
 -- Created by SQL::Translator::Producer::MySQL
--- Created on Tue Jul  2 11:17:35 2019
+-- Created on Wed Jul  3 11:33:19 2019
 -- 
 ;
 SET foreign_key_checks=0;
@@ -11,7 +11,7 @@ CREATE TABLE `event_log` (
   `id` integer NOT NULL auto_increment,
   `object_identifier` jsonb NOT NULL,
   `payload` jsonb NOT NULL,
-  `environmental_data` jsonb NULL,
+  `tags` jsonb NULL,
   `type` text NULL,
   `timestamp` timestamptz NOT NULL DEFAULT NOW(),
   PRIMARY KEY (`id`)
--- lib/auto/OpusVL/EventLog/Schema/sql/SQLite/deploy/1/001-auto.sql
+++ lib/auto/OpusVL/EventLog/Schema/sql/SQLite/deploy/1/001-auto.sql
@@ -1,6 +1,6 @@
 -- 
 -- Created by SQL::Translator::Producer::SQLite
--- Created on Tue Jul  2 11:17:35 2019
+-- Created on Wed Jul  3 11:33:19 2019
 -- 
 
 ;
@@ -12,7 +12,7 @@ CREATE TABLE event_log (
   id INTEGER PRIMARY KEY NOT NULL,
   object_identifier jsonb NOT NULL,
   payload jsonb NOT NULL,
-  environmental_data jsonb,
+  tags jsonb,
   type text,
   timestamp timestamptz NOT NULL DEFAULT NOW()
 );
